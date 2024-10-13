7728c43d632871ca635ed9b51d36f5a4e59c2aa0
Matthieu <matthieu.felten22@gmail.com>
2024-10-13 16:36:42 +0200

    Add showed header

---
+++ script.js
@@ -0,0 +1,12 @@
+const header = document.getElementById("header")
+const body = document.querySelector("body")
+const html = document.querySelector("html")
+
+body.onscroll = function (e) {
+    if (html.scrollTop > header.offsetHeight * 1.2) {
+        header.classList.add("showed")
+    }
+    else {
+        header.classList.remove("showed")
+    }
+}

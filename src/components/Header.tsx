d3f4d2b6f50cfa0f695ca3082b9f7c11b7e51fcb
MAT06mat Desktop <mat06mat22@gmail.com>
2024-10-21 22:35:17 +0200

    Update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,23 +18,32 @@ function Header() {
             }
         };
 
+    const [menuState, setMenuState] = useState(false);
+
+    function openMenu() {
+        setMenuState(true);
+    }
+
+    function closeMenu() {
+        setMenuState(false);
+    }
+
     return (
         <header className={showed ? "showed" : ""} id="header">
             <nav>
-                <label htmlFor="menu-btn" className="btn menu-btn">
+                <div className="btn menu-btn" onClick={openMenu}>
                     <svg viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
                         <path d="M3 6L37 6" />
                         <path d="M3 34L37 34" />
                         <path d="M3 20L37 20" />
                     </svg>
-                </label>
+                </div>
                 <Link to="" className="logo">
                     <span className="complementary">ULTRA</span>ORDINAIRE
                 </Link>
-                <input type="radio" name="slider" id="close-btn" />
-                <input type="radio" name="slider" id="menu-btn" />
-                <ul className="nav-links">
-                    <label htmlFor="close-btn" className="btn close-btn">
+                <div className="background-button" onClick={closeMenu}></div>
+                <ul className={menuState ? "nav-links open" : "nav-links"}>
+                    <div className="btn close-btn" onClick={closeMenu}>
                         <svg
                             viewBox="0 0 40 40"
                             xmlns="http://www.w3.org/2000/svg"
@@ -42,66 +51,111 @@ function Header() {
                             <path d="M3 37L37 3" />
                             <path d="M37 37L3 3" />
                         </svg>
-                    </label>
+                    </div>
                     <li>
-                        <Link to="conferences">CONFERENCES</Link>
+                        <Link to="conferences" onClick={closeMenu}>
+                            CONFERENCES
+                        </Link>
                         <ul className="drop-menu">
                             <li>
-                                <Link to="conferences/mental">MENTAL</Link>
+                                <Link
+                                    to="conferences/mental"
+                                    onClick={closeMenu}
+                                >
+                                    MENTAL
+                                </Link>
                             </li>
                             <li>
-                                <Link to="conferences/physique">PHYSIQUE</Link>
+                                <Link
+                                    to="conferences/physique"
+                                    onClick={closeMenu}
+                                >
+                                    PHYSIQUE
+                                </Link>
                             </li>
                             <li>
-                                <Link to="conferences/entourage">
+                                <Link
+                                    to="conferences/entourage"
+                                    onClick={closeMenu}
+                                >
                                     ENTOURAGE
                                 </Link>
                             </li>
                         </ul>
                     </li>
                     <li>
-                        <Link to="reseau">RESEAU</Link>
+                        <Link to="reseau" onClick={closeMenu}>
+                            RESEAU
+                        </Link>
                         <ul className="drop-menu">
                             <li>
-                                <Link to="reseau/secteurs-d-intervention">
+                                <Link
+                                    to="reseau/secteurs-d-intervention"
+                                    onClick={closeMenu}
+                                >
                                     SECTEURS D'INTERVENTION
                                 </Link>
                             </li>
                             <li>
-                                <Link to="reseau/coordination-des-interventions">
+                                <Link
+                                    to="reseau/coordination-des-interventions"
+                                    onClick={closeMenu}
+                                >
                                     COORDINATION DES INTERVENTIONS
                                 </Link>
                             </li>
                             <li>
-                                <Link to="reseau/optimisation-des-conditions-d-interventions">
+                                <Link
+                                    to="reseau/optimisation-des-conditions-d-interventions"
+                                    onClick={closeMenu}
+                                >
                                     OPTIMISATION DES CONDITIONS D'INTERVENTION
                                 </Link>
                             </li>
                         </ul>
                     </li>
                     <li>
-                        <Link to="associations">ASSOCIATIONS</Link>
+                        <Link to="associations" onClick={closeMenu}>
+                            ASSOCIATIONS
+                        </Link>
                         <ul className="drop-menu last">
                             <li>
-                                <Link to="associations/missions">MISSIONS</Link>
+                                <Link
+                                    to="associations/missions"
+                                    onClick={closeMenu}
+                                >
+                                    MISSIONS
+                                </Link>
                             </li>
                             <li>
-                                <Link to="associations/projets-sportifs-solidaires-et-de-cohesion">
+                                <Link
+                                    to="associations/projets-sportifs-solidaires-et-de-cohesion"
+                                    onClick={closeMenu}
+                                >
                                     PROJETS SPORTIFS SOLIDAIRES ET DE COHESION
                                 </Link>
                             </li>
                             <li>
-                                <Link to="associations/projets-innovants">
+                                <Link
+                                    to="associations/projets-innovants"
+                                    onClick={closeMenu}
+                                >
                                     PROJETS INNOVANTS
                                 </Link>
                             </li>
                             <li>
-                                <Link to="associations/creation-course-solidaire">
+                                <Link
+                                    to="associations/creation-course-solidaire"
+                                    onClick={closeMenu}
+                                >
                                     CREATION COURSE SOLIDAIRE
                                 </Link>
                             </li>
                             <li>
-                                <Link to="associations/handicap-et-dependance">
+                                <Link
+                                    to="associations/handicap-et-dependance"
+                                    onClick={closeMenu}
+                                >
                                     MISE EN LUMIERE DU HANDICAP ET DE LA
                                     DEPENDANCE
                                 </Link>

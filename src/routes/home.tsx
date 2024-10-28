2430482528b1383fc8f9cf696c8029d1497a939a
Matthieu <matthieu.felten22@gmail.com>
2024-10-28 16:36:42 +0100

    Update

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -14,7 +14,7 @@ function Home() {
                 />
                 <link rel="canonical" href="" />
             </Helmet>
-            <Section background="panorama.webp" noContainer>
+            <Section background="mb.jpg" noContainer>
                 <br />
                 <br />
                 <br />

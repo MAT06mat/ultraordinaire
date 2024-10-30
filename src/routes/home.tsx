58aa29d0dae7b9a04975c27dc9088fa93dd9983f
Matthieu <matthieu.felten22@gmail.com>
2024-10-30 12:33:52 +0100

    Update

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -302,11 +302,6 @@ function Home() {
                     expedita dicta excepturi sed eum magnam consequatur iste?
                 </p>
             </Section>
-            <Section>
-                <a href="/pdf_test.pdf" download={"/pdf_test.pdf"}>
-                    TELECHARGER LE PDF (test pour papi)
-                </a>
-            </Section>
         </>
     );
 }

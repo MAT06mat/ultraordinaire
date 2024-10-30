c0e0d7457b14ae931d4d41eeaeaed9fa979b96c2
Matthieu <matthieu.felten22@gmail.com>
2024-10-30 15:35:51 +0100

    Update

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -52,12 +52,12 @@ function Home() {
                         les plus difficiles au monde : <b>UTMB</b> et{" "}
                         <b>le grand raid de la réunion</b> (la Diagonale des
                         Fous). <br /> À chaque course, un dossard solidaire a
-                        été financé par des partenaires privées pour les
-                        recherches fondation maladies rares !
+                        été financé par des partenaires en faveur de la
+                        Fondation Maladies Rares!
                         <br />
                         La notion de <b>sport santé</b> est une base commune aux
                         3 sentiers développés sur ce site : aventures, réseau,
-                        associations. <br />
+                        association. <br />
                         Ce <b>professionnel hospitalier</b>, diplômé en activité
                         physique adaptée et aux multiples casquettes
                         professionnelles (enseignant/coordinateur APA,
@@ -105,15 +105,15 @@ function Home() {
                                     <p>
                                         La passion du sport individuel-collectif
                                         et indoor-outdoor a rythmé ma vie,
-                                        depuis les plus jeunes âges. Toutes ces
+                                        depuis le plus jeune âge. Toutes ces
                                         expériences motrices et sociales ont
                                         participé à la construction du sportif,
                                         du professionnel et de l’homme que je
-                                        suis aujourd’hui : un parcours de vie
-                                        ordinaire sans expérience du haut niveau
-                                        ou de l'élitisme mais en essayant d'être
-                                        la meilleure version de moi même sur
-                                        chaque nouvelle expérience initiée.
+                                        suis aujourd’hui : un parcours sportif
+                                        ordinaire en dehors de l’élitisme mais
+                                        en essayant d'être la meilleure version
+                                        de moi même sur chaque nouvelle
+                                        expérience initiée.
                                     </p>
                                 ),
                             },
@@ -125,8 +125,8 @@ function Home() {
                                         développer sa capacité à fédérer des
                                         énergies positives et compétentes est un
                                         véritable atout dans la construction
-                                        collective de son projet : donner du
-                                        sens à son projet action.
+                                        collective du projet : donner du sens à
+                                        son action.
                                     </p>
                                 ),
                             },
@@ -176,44 +176,14 @@ function Home() {
                             de rencontrer de nombreux professionnels dans des
                             domaines d’activités variées et un ensemble de
                             réseaux indispensables à la réussite des actions
-                            initiées. <br /> Le sport et Vous ça fait deux ?
+                            initiées. <br /> Le sport et vous ça fait deux ?
                             Peut être mais votre santé et vous ne font qu'un !{" "}
                             <br /> Vous avez besoin d’une collaboration ultra
                             ordinaire ? Vous êtes sur le bon sentier.
                         </p>
                     </div>
                     <div style={{ width: "30%" }}>
-                        <svg
-                            fill="#000000"
-                            height="800px"
-                            width="800px"
-                            version="1.1"
-                            id="Capa_1"
-                            xmlns="http://www.w3.org/2000/svg"
-                            xmlnsXlink="http://www.w3.org/1999/xlink"
-                            viewBox="0 0 275.072 275.072"
-                            xmlSpace="preserve"
-                        >
-                            <g>
-                                <path
-                                    d="M195.69,0H79.379c-9.884,0-17.926,8.05-17.926,17.945v239.192c0,9.89,8.042,17.935,17.926,17.935h116.31
-		c9.886,0,17.929-8.045,17.929-17.935V17.945C213.619,8.05,205.576,0,195.69,0z M76.454,61.934l122.165,0.003v133.423H76.454V61.934
-		z M79.379,15h116.31c1.615,0,2.929,1.321,2.929,2.945v28.991L76.454,46.934V17.945C76.454,16.321,77.766,15,79.379,15z
-		 M195.69,260.072H79.379c-1.586,0-2.926-1.344-2.926-2.935v-46.778h122.165v46.778C198.619,258.755,197.305,260.072,195.69,260.072
-		z"
-                                />
-                                <path
-                                    d="M137.536,217.458c-9.784,0-17.744,7.966-17.744,17.758c0,9.778,7.96,17.733,17.744,17.733
-		c9.785,0,17.745-7.955,17.745-17.733C155.281,225.424,147.32,217.458,137.536,217.458z M137.536,242.949
-		c-4.299,0-7.744-3.457-7.744-7.733c0-4.28,3.445-7.758,7.744-7.758c4.29,0,7.745,3.479,7.745,7.758
-		C145.281,239.492,141.826,242.949,137.536,242.949z"
-                                />
-                                <path
-                                    d="M122.45,35.967h30.174c2.761,0,5-2.239,5-5c0-2.761-2.239-5-5-5H122.45c-2.761,0-5,2.239-5,5
-		C117.45,33.728,119.688,35.967,122.45,35.967z"
-                                />
-                            </g>
-                        </svg>
+                        <img src="/images/mobile.svg" alt="" />
                         <h3>Contactez moi pour votre projet !</h3>
                         <button>Contactez moi maintenant</button>
                     </div>

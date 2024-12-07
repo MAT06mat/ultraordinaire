acc7dfff952c888102c1dc931e9f11b47ce1beb1
Matthieu <matthieu.felten22@gmail.com>
2024-12-07 18:44:01 +0100

    Text update

--- src/components/Action.tsx
+++ src/components/Action.tsx
@@ -12,27 +12,21 @@ function Action() {
                         de Julien
                     </h1>
                     <p>
-                        Quelque soit vos besoins, entreprise, établissement
-                        public et privé, association ou fondation, indépendant
-                        ou particulier, vous trouverez, peut être, sur les 2
-                        sentiers, un levier de discussion qui fait sens pour vos
-                        équipes ou pour vous même. Le sport et vous ça fait deux
-                        ?<br />
-                        <b>
-                            Peut-être mais votre santé et vous ne font qu’un !
-                        </b>
-                        <br />
-                        <br />
                         Vous avez besoin d’un intervenant, d’un accompagnement ?
                         <br />
-                        Vous êtes sur les bons sentiers.
+                        Quelque soit vos <b>besoins</b>, entreprise,
+                        établissement public et privé, association ou fondation,
+                        indépendant ou particulier, vous trouverez, peut être,
+                        sur les 2 sentiers, un <b>levier de discussion</b> qui
+                        fait sens pour vos équipes ou pour vous même. Le sport
+                        et vous ça fait deux ?<br />
+                        Peut-être mais <b>votre santé</b> et vous ne font qu’un
+                        !
                     </p>
                 </div>
                 <div>
                     <img src="/images/mobile.svg" alt="" />
-                    <h3 className="center">
-                        Contactez moi pour votre projet !
-                    </h3>
+                    <h3 className="center">Vous êtes sur le bon chemin</h3>
                     <button className="margin">Contactez moi maintenant</button>
                 </div>
             </div>

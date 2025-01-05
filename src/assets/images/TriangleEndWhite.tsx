6cbc495ba422b43105d543a58debcd1eabc5a85c
Matthieu <matthieu.felten22@gmail.com>
2025-01-05 13:34:09 +0100

    Patch a bug

--- src/assets/images/TriangleEndWhite.tsx
+++ src/assets/images/TriangleEndWhite.tsx
@@ -5,6 +5,7 @@ function TriangleEndWhite() {
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
+            style={{ marginBottom: "-1px" }}
         >
             <g
                 id="Page-1"
--- src/assets/images/TriangleStartWhite.tsx
+++ src/assets/images/TriangleStartWhite.tsx
@@ -5,6 +5,7 @@ function TriangleStartWhite() {
             version="1.1"
             xmlns="http://www.w3.org/2000/svg"
             xmlnsXlink="http://www.w3.org/1999/xlink"
+            style={{ marginTop: "-1px" }}
         >
             <g
                 id="Page-1"

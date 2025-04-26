deea1271d2555db4f614bb4af150e82c70bd50e1
Matthieu <matthieu.felten22@gmail.com>
2025-04-26 10:58:33 +0200

    Add pulse animation to the videso button

--- src/assets/images/PlayButton2.tsx
+++ src/assets/images/PlayButton2.tsx
@@ -1,7 +1,10 @@
 function PlayButton2() {
     return (
-        <svg viewBox="0 0 40 43" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g>
+        <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <g transform="translate(20,20)">
+                <circle id="pulse" r="20" />
+            </g>
+            <g transform="translate(1.25,0)">
                 <path d="M34 18.2679C35.3333 19.0377 35.3333 20.9623 34 21.7321L11.5 34.7224C10.1667 35.4922 8.5 34.53 8.5 32.9904V7.00962C8.5 5.47002 10.1667 4.50777 11.5 5.27757L34 18.2679Z" />
             </g>
         </svg>
